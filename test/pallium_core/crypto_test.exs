605f06c02a09f301b098f8dbb24219e1f4ba7ef4
Dmitry Zhelnin <dmitry.zhelnin@gmail.com>
2018-09-17 20:35:50 +0300

    fix crypto test

--- test/pallium_core/crypto_test.exs
+++ test/pallium_core/crypto_test.exs
@@ -6,6 +6,6 @@ defmodule PalliumCore.CryptoTest do
   test "encodes and decodes map" do
     map = %{a: "hello", b: "world"}
     encoded = map |> Crypto.encode_map()
-    assert map = Crypto.decode_map(encoded)
+    assert map == Crypto.decode_map(encoded)
   end
 end
